845a0e1b40b5238d43b603b289833233e95af132
Sourav Das <iamsouravdaswade420@gmail.com>
2024-06-01 22:55:01 +0530

    Initial Commit

---
+++ twitter_clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter_clone
+//
+//  Created by sourav das on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
